c99906c7dca2eb6179b2d19163865127ea1e5efa
isglx69 <isglx69@yahoo.com>
2018-11-24 15:36:47 +0200

    Structura de meniuri a aplicatiei si metode - 5

---
+++ src/main/java/Car/CarConsoleWriter.java
@@ -0,0 +1,12 @@
+package Car;
+
+public class CarConsoleWriter {
+
+        public void displayCarData(Car car) {
+            System.out.println(car.getCarCode() + "\t" +
+                    car.getModel() + "\t" +
+                    car.getVersionType() + "\t" +
+                    car.getColor() + "\t" +
+                    car.getPrice());
+        }
+}
---
+++ src/main/java/Car/CarStorage.java
@@ -0,0 +1,53 @@
+package Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarStorage extends client.GenericStore<Car> {
+
+    private List<Car> carList = new ArrayList();
+
+    @Override
+    public Car add(Car value) {
+        value.setCarCode(generateId());
+        carList.add(value);
+        return value;
+    }
+
+    private int generateId() {
+        int max = 0;
+                for(Car car : carList) {
+                    if(max < car.getCarCode()) {
+                        max = car.getCarCode();
+                    }
+                }
+        return max + 1;
+    }
+
+    @Override
+    public void delete(Car value) {
+        carList.remove(value);
+    }
+
+    @Override
+    public void update(Car value) {
+        Car oldCar = getById(value.getCarCode());
+        carList.remove(oldCar);
+        carList.add(value);
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return carList;
+    }
+
+    @Override
+    public Car getById(int id) {
+        for (Car c : carList) {
+            if(c.getCarCode() == id) {
+                return c;
+            }
+        }
+        return null;
+    }
+}
