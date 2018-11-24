6414b5ae9af6eded6d7b5dfcb47e189ef20448a9
Vasile Huian <vasile.huian@gmail.com>
2018-11-24 10:17:41 +0200

    Vasile  modificari CarCLI

--- src/main/java/Car/CarService.java
+++ src/main/java/Car/CarService.java
@@ -6,39 +6,39 @@ import java.util.List;
 
 public class CarService {
 
-    public static List<Car> carList = StockMenu.carList;
-    Car car = CarCLI.readCar();
-
-    public static void getAllModels{
-
-    }
-
-    public static List<Car> addCar(Car car) {
-        boolean ok = true;
-        while (ok) {
-            if (car.getCarCode() == 0) {    // adaugam masini pina introducem codul zero
-                ok = false;
-            } else {
-                if (!seekCar(car)) {
-                    carList.add(car);
-                    System.out.println("Masina " + car.getCarCode()+ " adaugata cu succes.");
-                    System.out.println();
-                } else {
-                    System.out.println("Codul " + car.getCarCode() + " exista deja. NU a fost adaugat.");
-                }
-                car = CarCLI.readCar();
-            }
-        }
-        return carList;
-    }
-
-    public static boolean seekCar(Car car) {
-        boolean found = false;
-        for(Car c: carList) {
-            if (c.getCarCode() == car.getCarCode()) {
-                found = true;
-            }
-        }
-        return found;
-    }
+//    public static List<Car> carList = StockMenu.carList;
+//    Car car = CarCLI.readCar();
+//
+//    public static void getAllModels{
+//
+//    }
+//
+//    public static List<Car> addCar(Car car) {
+//        boolean ok = true;
+//        while (ok) {
+//            if (car.getCarCode() == 0) {    // adaugam masini pina introducem codul zero
+//                ok = false;
+//            } else {
+//                if (!seekCar(car)) {
+//                    carList.add(car);
+//                    System.out.println("Masina " + car.getCarCode()+ " adaugata cu succes.");
+//                    System.out.println();
+//                } else {
+//                    System.out.println("Codul " + car.getCarCode() + " exista deja. NU a fost adaugat.");
+//                }
+//                car = CarCLI.readCar();
+//            }
+//        }
+//        return carList;
+//    }
+//
+//    public static boolean seekCar(Car car) {
+//        boolean found = false;
+//        for(Car c: carList) {
+//            if (c.getCarCode() == car.getCarCode()) {
+//                found = true;
+//            }
+//        }
+//        return found;
+//    }
 }
