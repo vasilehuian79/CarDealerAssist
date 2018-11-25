fbb249057f94b4120e18a5c66ffde44ab458cf1c
Vasile Huian <vasile.huian@gmail.com>
2018-11-25 12:30:08 +0200

    Actualizare delete car

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        CarDealerAssistent cda = new CarDealerAssistent("F:\\iosif diverse\\Curs JAVA\\SDAJavaProjects\\CarDealerAssist/Dealer.txt");
+        CarDealerAssistent cda = new CarDealerAssistent("D:\\SDA\\CarDealerAssist/Dealer.txt");
         cda.start();
     }
 }
