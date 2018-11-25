172907df644eb77c3f1f1dc53270ee4635ae935b
Victor Corobceanu <victor.corobceanu@yahoo.ro>
2018-11-25 09:39:54 +0200

    Remove client package + restore ClientService and ClientStorage

--- src/main/java/clientv2/ClientService.java
+++ src/main/java/clientv2/ClientService.java
@@ -1,40 +1,28 @@
 package clientv2;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ClientService {
+    ClientConsoleReader clientReader = new ClientConsoleReader();
+    ClientStorage clientStorage = new ClientStorage();
 
-    public void start() {
-        Client client = new Client(ClientCLI.generateRandomClientId());
-        ClientCLI cli = new ClientCLI();
-        cli.getClientData(client);
-        this.addClient(client);
+    public Client getClientById(int id){
+        return clientStorage.getById(id);
     }
 
-    public boolean addClient(Client client) {
-
-
-
-
-
-        // Save the client on the database/ file
-        return true;
+    public List<Client> getAllClients(){
+        return clientStorage.getAll();
     }
 
-    public Client findClient(int clientCode) {
-        // Must read from database/ file based on client code and will return the Client
-        // If database is used this should be the SQL query:
-        // SELECT * from ClientTable WHERE ClientTable.clientCode = clientCode
-        return new Client(clientCode);
+    public void addClient(Client client){
+        clientStorage.add(client);
     }
 
-    public List<Client> displayAllClients() {
-        // If you are using a Database, do a SELECT * clients
-        // If you are using a TextFile, get all clients from the .txt/ .json file
-        List<Client> list = new ArrayList<Client>();
-        // You will need to get clients and add these to the bellow list in the following manner:
-        // list.add(findClient(clientCode));
-        return list;
+    public void deleteClient(Client client){
+        clientStorage.delete(client);
+    }
+
+    public void updateClient(Client client){
+        clientStorage.update(client);
     }
 }
--- src/main/java/clientv2/ClientStorage.java
+++ src/main/java/clientv2/ClientStorage.java
@@ -1,22 +1,54 @@
 package clientv2;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ClientStorage {
+public class ClientStorage extends GenericStore<Client> {
+    private List<Client> clientList = new ArrayList<>();
 
-    List<Client> clientsList =  ArrayList<Client>();
-    clientsList
+    @Override
+    public Client add(Client value) {
+        value.setClientCode(generateId());
+        clientList.add(value);
+        return value;
+    }
 
+    private int generateId(){
+        int max = 0;
+        for (Client client:clientList){
+            if(max<client.getClientCode()){
+                max=client.getClientCode();
+            }
+        }
+        return max+1;
+    }
 
+    @Override
+    public void delete(Client value) {
+        clientList.remove(value);
 
+    }
 
+    @Override
+    public void update(Client value) {
+        Client oldClient = getById(value.getClientCode());
+        clientList.remove(oldClient);
+        clientList.add(value);
 
+    }
 
+    @Override
+    public List<Client> getAll() {
+        return clientList;
+    }
 
-
-
-
-
-
-
+    @Override
+    public Client getById(int id) {
+        for(Client client : clientList){
+            if(client.getClientCode() == id){
+                return client;
+            }
+        }
+        return null;
+    }
 }
